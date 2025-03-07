de7bfd945a2757fcd2be15d5aa637021d3002c90
gustavosx1 <gustavosantos.kng@gmail.com>
2025-03-07 16:00:12 -0300

    funcionario

---
+++ QrMeal/Principal/sobreFunc.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Restaurante Universitário</title>
+    <link rel="stylesheet" href="../style.css">
+    <?php include '../Inicio/config.php' ?>
+</head>
+
+<body>
+    <div class="topo padbot">
+        <div class="voltar">
+            <a href="menuFunc.php">
+                <img src="../midia/voltar.png" alt="">
+                <p>Voltar</p>
+            </a>
+        </div>
+    </div>
+    <div class="sobre">
+        <img src="../midia/QrMeal1.png" alt="">
+    </div>
+    <div class="info menu white" style="padding-top: 40%; height:60% !important">
+        <h3>Sobre</h3>
+        <p id="descricao">Nosso sistema é responsável por realizar a venda digital dos tickets para refeição no restaurante do Instituto Federal do Maranhão - Campus Monte Castelo</p>
+    </div>
+</body>
+
+</html>
