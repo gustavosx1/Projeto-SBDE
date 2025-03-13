4cb8bff9e6afb01ed161dd9ec09f7d53e0feb502
franc <francielealves095@gmail.com>
2025-03-13 10:41:48 -0300

    atualização cadastro

--- QrMeal/Inicio/cadastro.php
+++ QrMeal/Inicio/cadastro.php
@@ -2,16 +2,17 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     require 'conexao.php';
 
+    $matricula = $_POST['matricula']; // Captura a matrícula do formulário
     $nome = $_POST['nome'];
     $email = $_POST['email'];
-    $matricula = $_POST['matricula'];
-    $perfil = $_POST['perfil'];
+    $telefone = $_POST['telefone'];
+    $tipoAuxilio = $_POST['tipoAuxilio'];
     $senha = password_hash($_POST['senha'], PASSWORD_BCRYPT);
-    $status = 1;
 
     try {
-        $stmt = $pdo->prepare("INSERT INTO usuarios (nome, email, matricula, senha, perfil, status) VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$nome, $email, $matricula, $senha, $perfil, $status]);
+        // Insere o novo usuário com a matrícula
+        $stmt = $pdo->prepare("INSERT INTO pessoa (matricula, nome, email, telefone, tipoAuxilio, senha) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->execute([$matricula, $nome, $email, $telefone, $tipoAuxilio, $senha]);
 
         header("Location: login.php");
         exit();
@@ -50,24 +51,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <p class="erro"><?php echo $erro; ?></p>
         <?php endif; ?>
         <form method="POST">
+            <label for="matricula">Matrícula:</label>
+            <input type="text" id="matricula" name="matricula" required>
+
             <label for="nome">Nome:</label>
             <input type="text" id="nome" name="nome" required>
 
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required>
 
-            <label for="codigo">Matrícula/Código:</label>
-            <input type="text" id="codigo" name="codigo" required>
+            <label for="telefone">Telefone:</label>
+            <input type="number" id="telefone" name="telefone">
+
+            <label for="tipoAuxilio">Possui auxílio?:</label>
+            <select id="tipoAuxilio" name="tipoAuxilio">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
 
             <label for="senha">Senha:</label>
             <input type="password" id="senha" name="senha" required>
-
-            <label for="perfil">Perfil:</label>
-            <select class="perfil" id="perfil" name="perfil" required>
-                <option value="1">Administrador</option>
-                <option value="2">Funcionário</option>
-                <option value="3">Estudante</option>
-            </select>
 
             <p class="aviso">Já tem uma conta? <a href="login.php">Faça login</a></p>
             <button class="btwhite" type="submit">Cadastrar</button>
